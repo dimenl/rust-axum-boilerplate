f2b63f569e82089bc5f694bbbdeb2733aed84de8
Darshan Sithan <40591538+darsh0230@users.noreply.github.com>
2025-07-02 23:12:44 +0530

    make log directory configurable

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,6 @@
 pub mod constants;
 
-pub use constants::{ACCESS_TOKEN, DATABASE_URL, BCRYPT_COST, TOKEN_EXPIRATION_SECS};
+pub use constants::{ACCESS_TOKEN, BCRYPT_COST, DATABASE_URL, LOG_DIR, TOKEN_EXPIRATION_SECS};
 pub mod guards;
 pub mod jwt;
 pub mod logging;
