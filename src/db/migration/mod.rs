d5aca30d326ed9864eff3f39e4c6a86864d356cc
Darshan Sithan <40591538+darsh0230@users.noreply.github.com>
2025-07-02 11:27:05 +0530

    Add SeaORM entities and migrations

--- src/db/migration/mod.rs
+++ src/db/migration/mod.rs
@@ -1,1 +1,12 @@
-// Database migrations will be defined here
+pub use sea_orm_migration::prelude::*;
+
+mod m20240821_174402_users;
+
+pub struct Migrator;
+
+#[async_trait::async_trait]
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        vec![Box::new(m20240821_174402_users::Migration)]
+    }
+}
